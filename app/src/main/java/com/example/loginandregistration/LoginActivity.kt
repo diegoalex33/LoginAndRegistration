851d2b0a67596a00f6577f88a8f5ec69b0338f3f
per2 <diegoalexwilliams33@gmail.com>
2021-11-29 09:51:49 -0800

    Fix register

--- app/src/main/java/com/example/loginandregistration/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.loginandregistration
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.loginandregistration.databinding.ActivityLoginBinding
 
 
@@ -13,6 +16,15 @@ class LoginActivity : AppCompatActivity() {
     companion object {
         val EXTRA_USERNAME = "username"
         val EXTRA_PASSWORD = "password"
+    }
+
+    val startRegistrationForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            // Handle the Intent
+            binding.editTextLoginUsername.setText(intent?.getStringExtra(EXTRA_USERNAME))
+            binding.editTextLoginPassword.setText(intent?.getStringExtra(EXTRA_PASSWORD))
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +40,11 @@ class LoginActivity : AppCompatActivity() {
                 putExtra(EXTRA_PASSWORD, password)
             }
 
-            startActivity(registrationIntent)
+            //startActivity(registrationIntent)
+
+            //Can also launch startRegistrationForResult
+            startRegistrationForResult.launch(registrationIntent)
+
         }
     }
 }
--- app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -1,5 +1,7 @@
 package com.example.loginandregistration
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -18,6 +20,20 @@ class RegistrationActivity : AppCompatActivity() {
         val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME)
         val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD)
         Toast.makeText(this, "user:$username pwd:$password", Toast.LENGTH_SHORT).show()
+
+        binding.buttonRegistrationRegister.setOnClickListener {
+            //TODO: verify the information they entered is valid
+
+            //Return to login screen and prefill username and password
+
+            var returnToLogin = Intent().apply {
+              putExtra(LoginActivity.EXTRA_USERNAME, binding.editTextRegistrationUsername.text.toString())
+              putExtra(LoginActivity.EXTRA_PASSWORD, binding.editTextRegistrationPassword.text.toString())
+            }
+            setResult(Activity.RESULT_OK, returnToLogin)
+            finish()
+
+        }
 
     }
 }
